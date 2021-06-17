c309afbd9324f5c32d9b63f58179b50a7a921d17
Jan Varho <jan@varho.org>
2021-06-17 17:12:46 +0300

    Do not create trigger response lambda

--- packages/serverless-components/nextjs-component/src/component.ts
+++ packages/serverless-components/nextjs-component/src/component.ts
@@ -738,7 +738,6 @@ class NextjsComponent extends Component {
       maxTTL: 31536000,
       allowedHttpMethods: ["HEAD", "GET"],
       "lambda@edge": {
-        "origin-response": `${defaultEdgeLambdaOutputs.arn}:${defaultEdgeLambdaPublishOutputs.version}`,
         "origin-request": `${defaultEdgeLambdaOutputs.arn}:${defaultEdgeLambdaPublishOutputs.version}`
       }
     };
@@ -822,8 +821,7 @@ class NextjsComponent extends Component {
         ],
         "lambda@edge": {
           ...defaultLambdaAtEdgeConfig,
-          "origin-request": `${defaultEdgeLambdaOutputs.arn}:${defaultEdgeLambdaPublishOutputs.version}`,
-          "origin-response": `${defaultEdgeLambdaOutputs.arn}:${defaultEdgeLambdaPublishOutputs.version}`
+          "origin-request": `${defaultEdgeLambdaOutputs.arn}:${defaultEdgeLambdaPublishOutputs.version}`
         },
         compress: true
       },
